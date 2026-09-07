1ce6ddb8cf6e1956784bb2644ede445036362ce7
Monte George <montegeorgeiii@gmail.com>
2026-09-07 20:35:06 +0000

    Fix IntegrityError import placement

--- job_agent/pipeline.py
+++ job_agent/pipeline.py
@@ -17,9 +17,9 @@ from .providers.commonspirit import CommonSpiritProvider
 from .providers.bear_valley import BearValleyProvider
 from .repository import upsert_job, evaluation_exists
 from .settings_store import (
-from sqlalchemy.exc import IntegrityError
     seed_settings, get_bool, get_int, enabled_terms
 )
+from sqlalchemy.exc import IntegrityError
 
 RESUME_VERSION = "master-profile-v1"
 
